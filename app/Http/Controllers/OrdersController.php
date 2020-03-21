32b63e71453f8b99f56c9088fd486a8a54c93435
phpShine <phperShine@163.com>
2020-03-21 14:07:12 +0800

    用户界面-订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,6 +8,7 @@ use App\Models\UserAddress;
 use App\Models\Order;
 use Carbon\Carbon;
 use App\Exceptions\InvalidRequestException;
+use App\Jobs\CloseOrder;
 
 use Illuminate\Http\Request;
 
@@ -67,6 +68,20 @@ class OrdersController extends Controller
             return $order;
         });
 
+        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
+
         return $order;
+    }
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
     }
 }
