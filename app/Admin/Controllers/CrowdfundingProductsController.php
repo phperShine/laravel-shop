6971155c21c1a78046be3b583236cf372783ebd8
phpShine <phperShine@163.com>
2020-03-23 15:25:14 +0800

    管理后台商品控制器封装

--- app/Admin/Controllers/CrowdfundingProductsController.php
+++ app/Admin/Controllers/CrowdfundingProductsController.php
@@ -5,12 +5,42 @@ namespace App\Admin\Controllers;
 use App\Models\Category;
 use App\Models\CrowdfundingProduct;
 use App\Models\Product;
-use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 
-class CrowdfundingProductsController extends AdminController
+class CrowdfundingProductsController extends CommonProductsController
 {
+
+    public function getProductType()
+    {
+        return Product::TYPE_CROWDFUNDING;
+    }
+
+
+    protected function customGrid(Grid $grid)
+    {
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->column('crowdfunding.target_amount', '目标金额');
+        $grid->column('crowdfunding.end_at', '结束时间');
+        $grid->column('crowdfunding.total_amount', '目前金额');
+        $grid->column('crowdfunding.status', ' 状态')->display(function ($value) {
+            return CrowdfundingProduct::$statusMap[$value];
+        });
+    }
+
+
+    protected function customForm(Form $form)
+    {
+        // 众筹相关字段
+        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
+        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
+    }
+
     /**
      * Title for current resource.
      *
@@ -23,7 +53,7 @@ class CrowdfundingProductsController extends AdminController
      *
      * @return Grid
      */
-    protected function grid()
+    protected function grid_BAK()
     {
         $grid = new Grid(new Product);
 
@@ -56,39 +86,13 @@ class CrowdfundingProductsController extends AdminController
         return $grid;
     }
 
-    /**
-     * Make a show builder.
-     *
-     * @param mixed $id
-     * @return Show
-     */
-    protected function detail($id)
-    {
-        $show = new Show(Product::findOrFail($id));
-
-        $show->field('id', __('Id'));
-        $show->field('type', __('Type'));
-        $show->field('category_id', __('Category id'));
-        $show->field('title', __('Title'));
-        $show->field('description', __('Description'));
-        $show->field('image', __('Image'));
-        $show->field('on_sale', __('On sale'));
-        $show->field('rating', __('Rating'));
-        $show->field('sold_count', __('Sold count'));
-        $show->field('review_count', __('Review count'));
-        $show->field('price', __('Price'));
-        $show->field('created_at', __('Created at'));
-        $show->field('updated_at', __('Updated at'));
-
-        return $show;
-    }
 
     /**
      * Make a form builder.
      *
      * @return Form
      */
-    protected function form()
+    protected function form_BAK()
     {
         $form = new Form(new Product);
 
--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -9,8 +9,37 @@ use Encore\Admin\Grid;
 use Encore\Admin\Show;
 use App\Models\Category;
 
-class ProductsController extends AdminController
+class ProductsController extends CommonProductsController
 {
+
+    public function getProductType()
+    {
+        return Product::TYPE_NORMAL;
+    }
+
+
+    protected function customGrid(Grid $grid)
+    {
+        $grid->model()->with(['category']);
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->column('category.name', '类目');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
+    }
+
+
+    protected function customForm(Form $form)
+    {
+        // 普通商品没有额外的字段，因此这里不需要写任何代码
+    }
+
+
     /**
      * Title for current resource.
      *
@@ -23,7 +52,7 @@ class ProductsController extends AdminController
      *
      * @return Grid
      */
-    protected function grid()
+    protected function grid_BAK()
     {
         $grid = new Grid(new Product);
         $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
@@ -63,7 +92,7 @@ class ProductsController extends AdminController
      *
      * @return Form
      */
-    protected function form()
+    protected function form_BAK()
     {
         $form = new Form(new Product);
 
